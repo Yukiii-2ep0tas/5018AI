41f1c9888229ac6f43b61210b38b52ca4255877c
Yukiii-2ep0tas <76561110+Yukiii-2ep0tas@users.noreply.github.com>
2025-09-29 19:37:00 +0800

    start creating the playlist function

--- Assmt_2.py
+++ Assmt_2.py
@@ -3,7 +3,7 @@ import json
 
 f_API = open("./Cred/API_Key.txt")
 
-def catch_snippet(Username):
+def catch_channel_data(channel_id):
     api_service_name = "youtube"
     api_version = "v3"
 
@@ -12,13 +12,39 @@ def catch_snippet(Username):
     youtube = build(api_service_name, api_version, developerKey=api_key)
 
     request = youtube.channels().list(
-        part="snippet",
-        id = "UCw7SNYrYei7F5ttQO3o-rpA"
+        part="snippet,statistics",
+        id = channel_id
     )
     response = request.execute()
 
-    with open(f"./Saves/{Username}_snippet.json","w",encoding="utf-8") as f:
+    channel_name = response['items'][0]['snippet']['title']
+
+    with open(f"./Saves/{channel_name}_snippet.json","w",encoding="utf-8") as f:
         json.dump(response,f,indent=4)
+    return 0
+
+def get_description(json_filename):
+    with open(f"./Saves/{json_filename}") as f:
+        description = json.load(f)
+        print(description["items"][0]['snippet']['description'])
+    return 0
+
+def catch_playlist_data(channel_id,playlist_id):
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    api_key = f_API.readlines()
+
+    youtube = build(api_service_name, api_version, developerKey=api_key)
+
+    request = youtube.playlists().list(
+        channelID = channel_id,
+        playlistID = playlist_id,
+        part = "snippet,contentDetails"
+    )
+
+    respons = request.execute()
 
 if __name__ == "__main__":
-    catch_snippet("disneychannelanimation")
+    get_description("Disney Channel Animation_snippet.json")
+    catch_playlist_data("UCsT0YIqwnpJCM-mx7-gSA4Q")
