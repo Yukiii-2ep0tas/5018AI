e903d1ad232484134b70bcc1df09af17fa0f677b
Powerstation <Pstn@home>
2025-10-01 20:31:05 +0800

    10/1 update on powerstation

--- Assmt_2.py
+++ Assmt_2.py
@@ -4,7 +4,7 @@ import re
 
 f_API = open("./Cred/API_Key.txt")
 
-def catch_channel_data(channel_id):
+def catch_channel_data(channel_id):#获取频道数据并保存到json
     api_service_name = "youtube"
     api_version = "v3"
 
@@ -24,27 +24,13 @@ def catch_channel_data(channel_id):
         json.dump(response,f,indent=4)
     return 0
 
-def get_description(json_filename):
+def get_description(json_filename):#从保存的json文件中获取频道简介
     with open(f"./Saves/{json_filename}") as f:
-        description = json.load(f)
-        print(description["items"][0]['snippet']['description'])
-    return 0
+        description = json.load(f)["items"][0]['snippet']['description']
+        print(description)
+    return description
 
-def get_playlist_detail(json_filename,if_detailed):
-    with open(f"./Saves/{json_filename}") as f:
-        playlists = json.load(f)
-        playlist_amount = playlists['pageInfo']["totalResults"]
-        print(f"Total amount of playlists is :{playlist_amount}")
-        if if_detailed:
-            ordinal = 1
-            for item in playlists['items']:
-                create_date = re.match(r'^(.*?)T', item['snippet']['publishedAt']).group(1)
-                playlist_name = item['snippet']['title']
-                print(f'{ordinal}.Playlist Name is {playlist_name} and was created at {create_date}')
-                ordinal += 1
-    return 0
-
-def catch_playlist_data(channel_id):
+def catch_playlist_data(channel_id):#获取指定频道的所有播放列表信息并保存到json文件
     api_service_name = "youtube"
     api_version = "v3"
 
@@ -61,8 +47,24 @@ def catch_playlist_data(channel_id):
     with open(f"./Saves/{channel_id}_snippet.json","w",encoding="utf-8") as f:
         json.dump(response,f,indent=4)
 
+def get_playlist_detail(json_filename,if_detailed):#读取保存的json文件并输出该频道播放列表的总数
+    with open(f"./Saves/{json_filename}") as f:
+        playlists = json.load(f)
+        playlist_amount = playlists['pageInfo']["totalResults"]
+        print(f"Total amount of playlists is :{playlist_amount}")
+        if if_detailed:#通过if detailed 参数控制是否输出播放列表中每个视频的信息
+            ordinal = 1
+            for item in playlists['items']:
+                create_date = re.match(r'^(.*?)T', item['snippet']['publishedAt']).group(1)
+                playlist_name = item['snippet']['title']
+                print(f'{ordinal}.Playlist Name is {playlist_name} and was created at {create_date}')
+                ordinal += 1
+    return 0
+
+
+
 if __name__ == "__main__":
     # get_description("Disney Channel Animation_snippet.json")
     # catch_playlist_data(channel_id="UCsT0YIqwnpJCM-mx7-gSA4Q") #catch TS'channel
     # catch_playlist_data(channel_id="UC2jIjRE_1uvHNpRH3BSwu9Q") #catch CUHK MBA Channel
-    get_playlist_detail(json_filename='UC2jIjRE_1uvHNpRH3BSwu9Q_snippet.json',if_detailed=1)
+    # get_playlist_detail(json_filename='UC2jIjRE_1uvHNpRH3BSwu9Q_snippet.json',if_detailed=1)
