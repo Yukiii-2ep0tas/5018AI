5d6081fdc20a4c548550aacb98176f16d08f42e3
Powerstation <Pstn@home>
2025-10-11 00:00:57 +0800

    starting working on as3Q1

---
+++ Asg3/asgmt3_q1.py
@@ -0,0 +1,10 @@
+from google.cloud import language_v2
+import pandas as pd
+
+cred = ""
+
+client = language_v2.LanguageServiceClient()
+
+text = pd.read_csv("./Text/Q1_ECHO_COMMENT.csv",encoding='utf-8')
+
+print(text)
